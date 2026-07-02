77d3c4c5a148eea1a1ba8f78772d0f038c41b12d
Piyush Kumar <pkk8466@gmail.com>
2026-07-02 18:57:47 +0530

    Fix celery rename

--- backend/freshinbasket_core/celery.py
+++ backend/freshinbasket_core/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenmart_core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freshinbasket_core.settings')
 
-app = Celery('greenmart_core')
+app = Celery('freshinbasket_core')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
